a9d62d030f54f8bb1602c92ece04e500f204c849
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-06-01 07:32:37 +0100

    Created Methods to return movie by ObjectId & IdbmID

--- src/main/java/com/valleon/movies/MovieService.java
+++ src/main/java/com/valleon/movies/MovieService.java
@@ -1,18 +1,29 @@
 package com.valleon.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
-
     @Autowired
     private MovieRepository movieRepository;
+    private Movie movie;
 
-    public List<Movie> allMovies(){
+    public List<Movie> allMovies() {
         return movieRepository.findAll();
-
     }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> singleMovieImdb(String idbmId) {
+        return movieRepository.findMovieByImdbId(idbmId);
+    }
+
+
 }
